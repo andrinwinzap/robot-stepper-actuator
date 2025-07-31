a15979394fbab1220e00f99aa74ea70a350616ad
andrinwinzap <AndrinWinzap@proton.me>
2025-07-31 14:40:43 +0200

    refactor

--- include/config.h
+++ include/config.h
@@ -4,6 +4,7 @@
 #define I2C_SDA_GPIO 18
 #define I2C_SCL_GPIO 17
 #define I2C_PORT I2C_NUM_0
+#define I2C_FREQ_HZ 400000
 
 #define STEP_PIN GPIO_NUM_4
 #define DIR_PIN GPIO_NUM_5
@@ -18,8 +19,11 @@
 #define MAX_SPEED 6.28
 #define MAX_ACCELERATION 3.14
 
+#define INVERT_AS5600 false
 #define SPEED_FILTER_ALPHA 0.1f
 #define SPEED_DEADBAND 0.01f
+
+#define HALL_EFFECT_SENSOR_PIN 8
 
 #define KP 1.0f
 #define KI 0.0f
@@ -27,5 +31,8 @@
 #define KF 0.0f
 
 #define CONTROL_LOOP_FREQUENCY 100
+
+const char *robot_name = "robot";
+const char *joint_name = "joint_1";
 
 #endif // CONFIG_H
--- main/main.c
+++ main/main.c
@@ -7,6 +7,8 @@
 #include "esp_log.h"
 #include "esp_system.h"
 #include "driver/uart.h"
+#include "driver/gpio.h"
+#include "driver/i2c.h"
 
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
@@ -19,8 +21,6 @@
 #include <rmw_microros/rmw_microros.h>
 #include "esp32_serial_transport.h"
 
-#include "driver/gpio.h"
-#include "driver/i2c.h"
 #include "as5600.h"
 #include "stepper.h"
 #include "pid.h"
@@ -29,17 +29,17 @@
 #include "macros.h"
 #include "config.h"
 
+#define TOPIC_BUFFER_SIZE 64
+
 rcl_publisher_t position_publisher;
 std_msgs__msg__Float32 position_publisher_msg;
+char position_publisher_topic[TOPIC_BUFFER_SIZE];
 
 rcl_subscription_t position_subscriber;
 std_msgs__msg__Float32 position_subscriber_msg;
-
-volatile bool is_homed = false;
+char position_subscriber_topic[TOPIC_BUFFER_SIZE];
 
 static const char *TAG = "Actuator";
-
-typedef float (*target_provider_t)(void);
 
 typedef struct
 {
@@ -66,36 +66,30 @@ void position_subscriber_callback(const void *msgin)
     actuator.position_ctrl = pos;
 }
 
-void i2c_init(void)
+void i2c_bus_init(i2c_port_t i2c_num, gpio_num_t sda, gpio_num_t scl)
 {
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_SDA_GPIO,
-        .scl_io_num = I2C_SCL_GPIO,
+        .sda_io_num = sda,
         .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = scl,
         .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = 400000,
-        .clk_flags = 0,
+        .master.clk_speed = I2C_FREQ_HZ,
     };
-    i2c_param_config(I2C_PORT, &conf);
-    i2c_driver_install(I2C_PORT, conf.mode, 0, 0, 0);
+    ESP_ERROR_CHECK(i2c_param_config(i2c_num, &conf));
+    ESP_ERROR_CHECK(i2c_driver_install(i2c_num, conf.mode, 0, 0, 0));
 }
 
-void hall_init(void)
+void gpio_input_init(gpio_num_t pin)
 {
     gpio_config_t io_conf = {
-        .pin_bit_mask = (1ULL << GPIO_NUM_8),
+        .pin_bit_mask = (1ULL << pin),
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_ENABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_DISABLE,
     };
     gpio_config(&io_conf);
-}
-
-float get_position_ctrl(void)
-{
-    return actuator.position_ctrl;
 }
 
 float actuator_get_position(void)
@@ -105,7 +99,6 @@ float actuator_get_position(void)
 
 void pid_loop_task(void *param)
 {
-    target_provider_t get_target = (target_provider_t)param;
     float dt_ms = 1000 / CONTROL_LOOP_FREQUENCY;
 
     for (;;)
@@ -113,8 +106,7 @@ void pid_loop_task(void *param)
         as5600_update(&actuator.as5600);
 
         float feedback = as5600_get_position(&actuator.as5600);
-        float setpoint = get_target();
-        float control_signal = pid_update(&actuator.pid, setpoint, feedback);
+        float control_signal = pid_update(&actuator.pid, actuator.position_ctrl, feedback);
 
         stepper_set_velocity(&actuator.stepper, control_signal);
 
@@ -149,14 +141,14 @@ void micro_ros_task(void *arg)
         &position_publisher,
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-        "/position"));
+        position_publisher_topic));
 
     // create subscriber
     RCCHECK(rclc_subscription_init_default(
         &position_subscriber,
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-        "/position_ctrl"));
+        position_subscriber_topic));
 
     // create timer,
     rcl_timer_t timer;
@@ -202,10 +194,46 @@ void app_main(void)
 {
     ESP_LOGI(TAG, "Starting Setup...");
 
-    esp_log_level_set("pid", ESP_LOG_INFO);
-    esp_log_level_set("stepper", ESP_LOG_INFO);
-    esp_log_level_set("as5600", ESP_LOG_INFO);
-    esp_log_level_set("homing", ESP_LOG_INFO);
+    snprintf(position_publisher_topic, TOPIC_BUFFER_SIZE, "/%s/%s/get_position", robot_name, joint_name);
+    snprintf(position_subscriber_topic, TOPIC_BUFFER_SIZE, "/%s/%s/set_position", robot_name, joint_name);
+
+    i2c_bus_init(I2C_PORT,
+                 I2C_SDA_GPIO,
+                 I2C_SCL_GPIO);
+
+    as5600_init(
+        &actuator.as5600,
+        I2C_PORT,
+        AS5600_DEFAULT_ADDR,
+        SPEED_FILTER_ALPHA,
+        SPEED_DEADBAND,
+        GEAR_RATIO,
+        INVERT_AS5600,
+        false);
+
+    gpio_input_init(HALL_EFFECT_SENSOR_PIN);
+
+    stepper_init(
+        &actuator.stepper,
+        STEP_PIN,
+        DIR_PIN,
+        EN_PIN,
+        GEAR_RATIO,
+        STEPS_PER_REV,
+        MICROSTEPS,
+        MAX_SPEED,
+        MAX_ACCELERATION);
+
+    pid_init(
+        &actuator.pid,
+        KP,
+        KI,
+        KD,
+        1.0f / CONTROL_LOOP_FREQUENCY);
+
+    actuator.position_ctrl = 0.0f;
+
+    home(&actuator.stepper, &actuator.as5600);
 
 #if defined(RMW_UXRCE_TRANSPORT_CUSTOM)
     rmw_uros_set_custom_transport(
@@ -220,59 +248,22 @@ void app_main(void)
 #endif // RMW_UXRCE_TRANSPORT_CUSTOM
 
     xTaskCreatePinnedToCore(
+        pid_loop_task,
+        "control_loop",
+        16384,
+        NULL,
+        10,
+        NULL,
+        1);
+
+    xTaskCreatePinnedToCore(
         micro_ros_task,
         "uros_task",
-        16000,
+        16384,
         NULL,
         5,
         NULL,
         0);
-
-    actuator.position_ctrl = 0.0f;
-
-    stepper_init(
-        &actuator.stepper,
-        STEP_PIN,
-        DIR_PIN,
-        EN_PIN,
-        GEAR_RATIO,
-        STEPS_PER_REV,
-        MICROSTEPS,
-        MAX_SPEED,
-        MAX_ACCELERATION);
-
-    i2c_init();
-
-    as5600_init(
-        &actuator.as5600,
-        I2C_PORT,
-        AS5600_DEFAULT_ADDR,
-        SPEED_FILTER_ALPHA,
-        SPEED_DEADBAND,
-        GEAR_RATIO,
-        1,
-        false);
-
-    pid_init(
-        &actuator.pid,
-        KP,
-        KI,
-        KD,
-        1.0f / CONTROL_LOOP_FREQUENCY);
-
-    hall_init();
-
-    home(&actuator.stepper, &actuator.as5600);
-    is_homed = true;
-
-    xTaskCreatePinnedToCore(
-        pid_loop_task,
-        "control_loop",
-        8000,
-        (void *)get_position_ctrl,
-        10,
-        NULL,
-        1);
 
     ESP_LOGI(TAG, "Setup Complete");
 }
