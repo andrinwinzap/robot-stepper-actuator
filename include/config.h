2d5f0b388fb0e8d6531344b12d7868b38142a4e8
andrinwinzap <AndrinWinzap@proton.me>
2025-08-12 12:16:50 +0200

    add compile definition for configuration

--- include/config.h
+++ include/config.h
@@ -1,39 +1,20 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define I2C_SDA_GPIO 18
-#define I2C_SCL_GPIO 17
-#define I2C_PORT I2C_NUM_0
-#define I2C_FREQ_HZ 400000
-
-#define STEP_PIN GPIO_NUM_4
-#define DIR_PIN GPIO_NUM_5
-#define EN_PIN GPIO_NUM_6
-#define INVERT_STEPPER false
-
-#define GEAR_RATIO 13.0f
-#define STEPS_PER_REV 200
-#define MICROSTEPS 8
-
-#define ACTUATOR_MAX 3.14
-#define ACTUATOR_MIN -3.14
-#define MAX_SPEED 6.0
-#define MAX_ACCELERATION 3.14
-
-#define INVERT_AS5600 false
-#define SPEED_FILTER_ALPHA 0.1f
-#define SPEED_DEADBAND 0.01f
-
-#define HALL_EFFECT_SENSOR_PIN 8
-
-#define KP 1.0f
-#define KI 0.0f
-#define KD 0.0f
-#define KF 1.0f
-
-#define CONTROL_LOOP_FREQUENCY 100
-
-const char *robot_name = "robot";
-const char *joint_name = "joint_4";
+#if JOINT == 1
+#pragma message("Building for Joint 1")
+#include "joint_1.h"
+#elif JOINT == 2
+#pragma message("Building for Joint 2")
+#include "joint_2.h"
+#elif JOINT == 3
+#pragma message("Building for Joint 3")
+#include "joint_3.h"
+#elif JOINT == 4
+#pragma message("Building for Joint 4")
+#include "joint_4.h"
+#else
+#error "No configuration selected"
+#endif
 
 #endif // CONFIG_H
